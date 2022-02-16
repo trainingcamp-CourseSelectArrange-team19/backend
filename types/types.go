a10772490c894bcadd396dc1488e8336df2d2cdf
wakkana <wakkana@outlook.com>
2022-02-16 19:12:21 +0000

    remove sqlex

--- types/types.go
+++ types/types.go
@@ -11,23 +11,22 @@ type ErrNo = int
 const (
 	OK                 ErrNo = 0
 	ParamInvalid       ErrNo = 1  // 参数不合法
-	UserHasExisted     ErrNo = 2   // 该 Username 已存在
-	UserHasDeleted     ErrNo = 3   // 用户已删除
-	UserNotExisted     ErrNo = 4   // 用户不存在
-	WrongPassword      ErrNo = 5   // 密码错误
-	LoginRequired      ErrNo = 6   // 用户未登录
-	CourseNotAvailable ErrNo = 7   // 课程已满
-	CourseHasBound     ErrNo = 8   // 课程已绑定过
-	CourseNotBind      ErrNo = 9   // 课程未绑定过
-	PermDenied         ErrNo = 10   // 没有操作权限
-	StudentNotExisted  ErrNo = 11   // 学生不存在
-	CourseNotExisted   ErrNo = 12   // 课程不存在
-	StudentHasNoCourse ErrNo = 13  // 学生没有课程
-	StudentHasCourse   ErrNo = 14  // 学生有课程
+	UserHasExisted     ErrNo = 2  // 该 Username 已存在
+	UserHasDeleted     ErrNo = 3  // 用户已删除
+	UserNotExisted     ErrNo = 4  // 用户不存在
+	WrongPassword      ErrNo = 5  // 密码错误
+	LoginRequired      ErrNo = 6  // 用户未登录
+	CourseNotAvailable ErrNo = 7  // 课程已满
+	CourseHasBound     ErrNo = 8  // 课程已绑定过
+	CourseNotBind      ErrNo = 9  // 课程未绑定过
+	PermDenied         ErrNo = 10 // 没有操作权限
+	StudentNotExisted  ErrNo = 11 // 学生不存在
+	CourseNotExisted   ErrNo = 12 // 课程不存在
+	StudentHasNoCourse ErrNo = 13 // 学生没有课程
+	StudentHasCourse   ErrNo = 14 // 学生有课程
 
-	UnknownError       ErrNo = 255 // 未知错误
+	UnknownError ErrNo = 255 // 未知错误
 )
-
 
 type ResponseMeta struct {
 	Code ErrNo
@@ -41,8 +40,8 @@ type TMember struct {
 }
 
 type TCourse struct {
-	CourseID string
-	Name     string
+	CourseID  string
+	Name      string
 	TeacherID string
 }
 
@@ -242,7 +241,7 @@ type ScheduleCourseRequest struct {
 
 type ScheduleCourseResponse struct {
 	Code ErrNo
-	Data map[string]string   // key 为 teacherID , val 为老师最终绑定的课程 courseID
+	Data map[string]string // key 为 teacherID , val 为老师最终绑定的课程 courseID
 }
 
 type BookCourseRequest struct {
@@ -266,8 +265,3 @@ type GetStudentCourseResponse struct {
 		CourseList []TCourse
 	}
 }
-
-type SQLEx = string
-const (
-	Insert SQLEx = "INSERT INTO "
-)
